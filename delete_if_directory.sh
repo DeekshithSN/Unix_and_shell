a73df9352cf064c422cf454ce5c910f8bc0871d0
Deekshith SN <deekshithsn@gmail.com>
2020-08-15 19:42:33 +0530

    Update delete_if_directory.sh

--- delete_if_directory.sh
+++ delete_if_directory.sh
@@ -1,7 +1,9 @@
 read input
-entered value is $input 
+echo "entered value is $input"
 if [ -d $input ] ; then
 rm -rf $input
-else
+elif [  -f $input ] ; then 
 echo "its file"
+else
+echo "file or directory not found"
 fi
